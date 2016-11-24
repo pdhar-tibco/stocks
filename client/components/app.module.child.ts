2e5867c62ce8574585acd9a34e788a96edacf182
Pranab Dhar <pdhar@tibco.com>
2016-11-23 18:39:18 -0800

    Change routerLink and merge child routes to the module

--- client/components/app.module.child.ts
+++ client/components/app.module.child.ts
@@ -1,9 +1,10 @@
+import { RouterModule } from "@angular/router";
 import { NgModule }                          from "@angular/core";
 // import { BrowserModule }                     from "@angular/platform-browser";
 import { CommonModule }                      from "@angular/common";
 import { HttpModule }                        from "@angular/http";
 import { FormsModule }                       from "@angular/forms";
-import { appChildRouting }                   from "./app.routing.child";
+// import { appChildRouting }                   from "./app.routing.child";
 import { StocksService }                     from "../services/stocks";
 import { Manage }                            from "./manage";
 import { Summary }                           from "./summary";
@@ -19,7 +20,17 @@ import { StockApp }                          from "./app";
     CommonModule,
     HttpModule,
     FormsModule,
-    appChildRouting,
+    RouterModule.forChild([
+        {
+            path: "stocks",
+            component: StockApp,
+            children: [
+                { path: "Dashboard", component: Dashboard },
+                { path: "Manage", component: Manage },
+                { path: "", redirectTo: "Dashboard", pathMatch: "full" },
+            ]
+        },
+    ]),
   ],
   providers: [
   ],
